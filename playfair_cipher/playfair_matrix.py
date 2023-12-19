95ec9b60ed809df79e12f6356370d600fe7240b7
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-12-19 23:34:23 +0200

    enhance code of playfair_matrix.py

--- playfair_cipher/playfair_matrix.py
+++ playfair_cipher/playfair_matrix.py
@@ -1,22 +1,17 @@
+import numpy as np
+
+
 def create_matrix(key):
     key = key.upper()
     playfair_matrix = [[0 for _ in range(5)] for _ in range(5)]
     letters_added = []
-    row = 0
-    col = 0
 
     # add the key to the matrix
     for letter in key:
         if letter not in letters_added:
-            playfair_matrix[row][col] = letter
             letters_added.append(letter)
         else:
             continue
-        if col == 4:
-            col = 0
-            row += 1
-        else:
-            col += 1
 
     # Add the rest of the alphabet to the matrix
     for letter in range(65, 91):
